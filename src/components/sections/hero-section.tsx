1f73ded56a354632f1e0af22b6d07e1a7cdd87c6
Tj Tolliver <tjsownemail@gmail.com>
2025-05-13 04:45:02 +0000

    ok we're using the vibehero2.png as our background and we need to match the text a bit better to it. the cyan/teal is great but the red should be more purpple or pink

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 // Corrected image import path
-import heroImage from '@/components/img/vibehero.jpg';
+import heroImage from '@/components/img/vibehero2.png';
 
 interface HeroSectionProps {
   id: string;
